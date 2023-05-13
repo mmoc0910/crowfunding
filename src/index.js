a5fb2c8c97435baa4d664ea00db750ed451b459a
mmoc0910 <mmoc0910@gmail.com>
2023-05-13 19:30:08 +0700

    change scss to css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "./index.scss";
+import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store/configureStore";
 import { BrowserRouter } from "react-router-dom";
